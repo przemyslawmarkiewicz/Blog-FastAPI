0720cc9cfcf57ddc2aa7a19efdabcceb29435e17
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2021-04-08 01:37:07 +0200

    added show user endponit

--- blog/main.py
+++ blog/main.py
@@ -68,7 +68,7 @@ def blog_details(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@ app.post('/user')
+@ app.post('/user', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(
         name=request.name, email=request.email, password=Hash.bcrypt(request.password))
@@ -76,3 +76,12 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def user_details(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} is not availiable')
+    return user
