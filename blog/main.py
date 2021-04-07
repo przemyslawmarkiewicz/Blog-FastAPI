561e5caa5d70d5e7274700e7a632f25cd7f6d26f
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2021-04-08 01:17:18 +0200

    added user model

--- blog/main.py
+++ blog/main.py
@@ -65,3 +65,13 @@ def blog_details(id, response: Response, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Blog with id {id} is not availiable')
     return blog
+
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
