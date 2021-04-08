791357de1e18ae6bd1a53171ac64b5341fe822e4
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2021-04-08 02:00:59 +0200

    Added relationship between blog and user

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,15 +1,13 @@
 from pydantic import BaseModel
+from typing import List
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-
+class Blog(BlogBase):
     class Config:
         orm_mode = True
 
@@ -23,6 +21,16 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog]
+
+    class Config:
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
 
     class Config:
         orm_mode = True
